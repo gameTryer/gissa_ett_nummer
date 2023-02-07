4f3ba3e952fc840dd961100a56c036c974a5bff0
Adrian-Rola <116452700+Adrian-Rola@users.noreply.github.com>
2023-02-07 11:16:23 +0100

    new modes
    added new funktions

--- numbers.py
+++ numbers.py
@@ -7,11 +7,8 @@ def minmax_check():
     max_ges = int(input("Ange högsta nummer."))
     if min_ges == max_ges:
         print("lägsta och högsta gissning får inte vara sama. försök igen")
-        return minmax_check() 
+        minmax_check()
     return min_ges, max_ges
-    minmax_check() 
-    return min_ges, max_ges
-
 def normal():
     min_ges, max_ges = minmax_check()
     s_ges = random.randint(min_ges,max_ges)
@@ -21,7 +18,7 @@ def normal():
             i_ges = int(input("gissa ett nummer"))
         except ValueError:
             print("Ange ett heltal!")
-        continue
+            continue
         if i_ges == s_ges:   
             print("kan gratulera, du gissade på "+str(s_ges)," och det tog"+str(i+1),"gånger")
             print("tryck på en tangent för att fortsätta")
@@ -37,6 +34,7 @@ def challenge():
     min_ges, max_ges = minmax_check()
     s_ges = random.randint(min_ges, max_ges)
     i = 0
+    j = 0
     interval = int(input("Ange hur ofta det slumpmässiga numret ska ändras (i antal gissningar):"))
     while True:
         try:
@@ -44,30 +42,31 @@ def challenge():
         except ValueError:
             print("Ange ett heltal!")
             continue
-            if i_ges == s_ges:
-                print("kan gratulera, du gissade på "+str(s_ges)," och det tog"+str(i+1),"gånger")
-                print("tryck på en tangent för att fortsätta")
-                paus()
-                break
-            elif i_ges < s_ges:
-                print("högre")
-                i = i+1
-            elif i_ges > s_ges:
-                print("lägre")
-                i = i+1
-        if i % interval == 0: # ändrar slumpmässigt nummer var interval:te gång
-            s_ges = random.randint(min_ges, max_ges)
+        if i_ges == s_ges:   
+            print(f"kan gratulera, du gissade på {s_ges} och det tog {i+1}gånger. talet ändrades {j+1} gånger")
+            print("tryck på en tangent för att fortsätta")
+            paus()
+            break
+        elif i_ges < s_ges:
+            print("högre")
+            i = i+1
+        elif i_ges > s_ges:
+            print("lägre")
+            i = i+1
 while True:
     print("välj spelläge.")
     print("1. Normalt läge")
     print("2. utmanings läge")
     print("0. avsluta")
-    c = int(input("välj spelläge"))
-    if c == 1:
-        normal()
-    elif c == 2:
-        challenge()
-    elif c == 0:
-        break
-    else:
-        print("ojiltigt val")
+    try:
+        c = int(input("välj spelläge"))
+        if c == 1:
+            normal()
+        elif c == 2:
+            challenge()
+        elif c == 0:
+            break
+        else:
+            print("ojiltigt val")
+    except  ValueError:
+        print("stöds inte i denna meny")
