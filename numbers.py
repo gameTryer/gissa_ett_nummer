c38a66dc8bb231b331bf6b9a7f348212ed2f2e99
Hamdpac <adrian.rola1203@outlook.com>
2023-07-31 13:12:46 +0200

    changed the language promps to to english

--- numbers.py
+++ numbers.py
@@ -1,65 +1,70 @@
 import msvcrt
 import random
+
 def paus():
     msvcrt.getch()
+
 def minmax_check():
-    min_ges = int(input("Ange lägsta nummer."))
-    max_ges = int(input("Ange högsta nummer."))
+    min_ges = int(input("Enter the minimum number: "))
+    max_ges = int(input("Enter the maximum number: "))
     if min_ges == max_ges:
-        print("lägsta och högsta gissning får inte vara sama. försök igen")
+        print("The minimum and maximum guesses cannot be the same. Please try again.")
         minmax_check()
     return min_ges, max_ges
+
 def normal():
     min_ges, max_ges = minmax_check()
-    s_ges = random.randint(min_ges,max_ges)
+    s_ges = random.randint(min_ges, max_ges)
     i = 0
     while True:
         try:
-            i_ges = int(input("gissa ett nummer"))
+            i_ges = int(input("Guess a number: "))
         except ValueError:
-            print("Ange ett heltal!")
+            print("Please enter an integer!")
             continue
-        if i_ges == s_ges:   
-            print("kan gratulera, du gissade på "+str(s_ges)," och det tog"+str(i+1),"gånger")
-            print("tryck på en tangent för att fortsätta")
+        if i_ges == s_ges:
+            print(f"Congratulations, you guessed {s_ges} and it took you {i+1} attempts.")
+            print("Press any key to continue...")
             paus()
             break
         elif i_ges < s_ges:
-            print("högre")
-            i = i+1
+            print("Higher.")
+            i += 1
         elif i_ges > s_ges:
-            print("lägre")
-            i = i+1
+            print("Lower.")
+            i += 1
+
 def challenge():
     min_ges, max_ges = minmax_check()
     s_ges = random.randint(min_ges, max_ges)
     i = 0
     j = 0
-    interval = int(input("Ange hur ofta det slumpmässiga numret ska ändras (i antal gissningar):"))
+    interval = int(input("Enter how often the random number should change (in number of guesses): "))
     while True:
         try:
-            i_ges = int(input("gissa ett nummer"))
+            i_ges = int(input("Guess a number: "))
         except ValueError:
-            print("Ange ett heltal!")
+            print("Please enter an integer!")
             continue
-        if i_ges == s_ges:   
-            print(f"kan gratulera, du gissade på {s_ges} och det tog {i+1}gånger. talet ändrades {j+1} gånger")
-            print("tryck på en tangent för att fortsätta")
+        if i_ges == s_ges:
+            print(f"Congratulations, you guessed {s_ges} and it took you {i+1} attempts. The number changed {j+1} times.")
+            print("Press any key to continue...")
             paus()
             break
         elif i_ges < s_ges:
-            print("högre")
-            i = i+1
+            print("Higher.")
+            i += 1
         elif i_ges > s_ges:
-            print("lägre")
-            i = i+1
+            print("Lower.")
+            i += 1
+
 while True:
-    print("välj spelläge.")
-    print("1. Normalt läge")
-    print("2. utmanings läge")
-    print("0. avsluta")
+    print("Choose game mode:")
+    print("1. Normal Mode")
+    print("2. Challenge Mode")
+    print("0. Exit")
     try:
-        c = int(input("välj spelläge"))
+        c = int(input("Select game mode: "))
         if c == 1:
             normal()
         elif c == 2:
@@ -67,6 +72,6 @@ while True:
         elif c == 0:
             break
         else:
-            print("ojiltigt val")
-    except  ValueError:
-        print("stöds inte i denna meny")
+            print("Invalid choice")
+    except ValueError:
+        print("Not supported in this menu.")
